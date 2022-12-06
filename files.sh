8190a22ab25234e1914dacfc107f29dc2b97b0c1
Sarah M Brown <brownsarahm@uri.edu>
2022-12-06 14:23:01 -0500

    fix typo

--- files.sh
+++ files.sh
@@ -1,3 +1,3 @@
 grep "index" _review/*.md > _data/review_list.txt
 grep "index" _prepare/*.md > _data/prepare_list.txt
-grep "index" _practice*.md > _data/practice_list.txt
+grep "index" _practice/*.md > _data/practice_list.txt
