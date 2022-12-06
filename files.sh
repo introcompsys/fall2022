2a62d142e1503e0f4f8e05d620f1c4aa6fd46bd1
Sarah M Brown <brownsarahm@uri.edu>
2022-12-06 14:01:55 -0500

    add file table for kwlfilecheck

--- files.sh
+++ files.sh
@@ -1,3 +1,3 @@
-grep "index" _review/* | awk '{print $2}' >> activities/review-files.md
-grep "index" _prepare/* | awk '{print $2}' >> activities/prepare-files.md
-grep "index" _practice/* | awk '{print $2}' >> activities/practice-files.md
+grep "index" _review/*.md > _data/review_list.txt
+grep "index" _prepare/*.md > _data/prepare_list.txt
+grep "index" _practice*.md > _data/practice_list.txt
